39400ebe0b628d8076f3e18ff4bb6633f16c8c59
BrandonAllenGreen <green04@gmail.com>
2018-03-04 19:47:08 -0600

    script is working and returning zero if no args entered

--- args.js
+++ args.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 'use strict';
 
+// this program should accept at least one number as a command-line argument. The arguments should be summed up into a single value
+
+let [, , ...myArgs] = process.argv;
+
+let sum; 
+
+if (myArgs.length !== null) {
+  sum = myArgs.map(Number).reduce((a, b) => a + b, 0);
+} else {
+  sum = 0;
+}
+
+console.log(sum);
+
+
+
+  
+
